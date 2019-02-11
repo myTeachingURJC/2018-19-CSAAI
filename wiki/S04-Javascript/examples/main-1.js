2e0822365341fae5e002f4443c6e6e5bc31853ea
obijuan <juan@iearobotics.com>
2019-02-11 16:36:00 +0100

    S4: More images added

---
+++ wiki/S04-Javascript/examples/main-1.js
@@ -0,0 +1,8 @@
+
+//-- Punto de entrada: una vez cargada la página se llama a esta
+//-- funcion: ahí comienza nuestro programa principal
+function main()
+{
+  console.log("La página ya está cargada")
+  console.log("Ahora es seguro ejecutar el código js")
+}
