01036881c865a634b38ebf73bf99b97cd97e5e45
obijuan <juan@iearobotics.com>
2019-02-11 16:54:23 +0100

    S4: Template for the projects added

---
+++ wiki/S04-Javascript/examples/plantilla.js
@@ -0,0 +1,8 @@
+
+//-- Punto de entrada: una vez cargada la página se llama a esta
+//-- funcion: ahí comienza nuestro programa principal
+function main()
+{
+  console.log("Aquí comienza tu código JS...")
+  console.log("¡Que comiencen los juegos del JS!")
+}
