93a0591ef679981d52d4ddfc988bb373df9658da
obijuan <juan@iearobotics.com>
2019-02-24 20:01:03 +0100

    S06: More images added

---
+++ wiki/S06-Audio-video/examples/canvas-01.js
@@ -0,0 +1,10 @@
+function main()
+{
+  //-- Obtener el elemento canvas
+  var canvas = document.getElementById("display");
+
+  //-- Definir el tamaño del canvas
+  canvas.width = 200;
+  canvas.height = 100;
+
+}
