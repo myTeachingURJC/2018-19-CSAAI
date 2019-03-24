688f5aef049795004c63b6f6a3c5d47b1e158ba9
obijuan <juan@iearobotics.com>
2019-03-24 12:26:48 +0100

    L09: More examples added

---
+++ wiki/L09-practica-4-1/examples/video-01.js
@@ -0,0 +1,11 @@
+function main()
+{
+  console.log("Video 01...")
+
+  //-- Obtener el elemento de video
+  video1 = document.getElementById("video1")
+
+  //-- Establecer el tamaño del vídeo
+  video1.width = 200;
+  video1.height= 100;
+}
