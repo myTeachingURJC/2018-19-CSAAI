d4e753a1a3f292083948cac1df4d8bd1881e2943
obijuan <juan@iearobotics.com>
2019-04-03 11:24:04 +0200

    L11: Image added

--- wiki/L011-Practica-5-1/examples/filtro1.js
+++ wiki/L011-Practica-5-1/examples/filtro1.js
@@ -1,13 +1,22 @@
 function main() {
   console.log("En main()....")
 
+  //-- Acceso al objeto con la imagen
   var img = document.getElementById('imagesrc')
+
+  //-- Acceso al objeto con el canvas
   var canvas = document.getElementById('display');
 
+  //-- Se establece como tamaño del canvas el mismo
+  //-- que el de la imagen original
   canvas.width = img.width;
   canvas.height = img.height;
 
+  //-- Obtener el contexto del canvas para
+  //-- trabajar con el
   var ctx = canvas.getContext("2d");
 
+  //-- Situar la imagen original en el canvas
+  //-- No se han hecho manipulaciones todavia
   ctx.drawImage(img, 0,0);
 }
